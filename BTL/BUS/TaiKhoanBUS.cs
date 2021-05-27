0166539b60b37d560ff26104bd3d04c03e7a562b
hanguyentien2000 <hanguyentien2000@gmail.com>
2021-05-27 20:09:49 +0700

    Update QuanLyTaiKhoan

--- BTL/BUS/TaiKhoanBUS.cs
+++ BTL/BUS/TaiKhoanBUS.cs
@@ -33,11 +33,15 @@ namespace BTL.BLL
             TaiKhoanDTO tk = new TaiKhoanDTO();
             tk.Username = dt.Rows[0].ItemArray[0].ToString();
             tk.Password = dt.Rows[0].ItemArray[1].ToString();
-            tk.LoaiTaiKhoan = dt.Rows[0].ItemArray[2].ToString();
+            //tk.LoaiTaiKhoan = dt.Rows[0].ItemArray[2].ToString();
             tk.MaNhanVien = int.Parse(dt.Rows[0].ItemArray[3].ToString());
             return tk;
         }
 
-     
+        public void AddTaiKhoan(string TenDangNhap, string MatKhau, int LoaiTaiKhoan, int MaNV)
+        {
+            string sql = "Insert into dbo.TAIKHOAN values('" + TenDangNhap + "','" + MatKhau + "','" + LoaiTaiKhoan + "','" + MaNV + "')";
+            data.ExecuteNonQuery(sql);
+        }
     }
 }
